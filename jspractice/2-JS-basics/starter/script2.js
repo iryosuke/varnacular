c161e7c3b8ffa37b419230d7f808687782d8ba01
Ryosuke Oshida <ryosukeoshida@Ryosukes-MacBook-Pro-2.local>
2020-04-26 01:53:50 +0900

    fibonacci series added

--- jspractice/2-JS-basics/starter/script2.js
+++ jspractice/2-JS-basics/starter/script2.js
@@ -32,13 +32,13 @@ console.log(isDesigner)
 
 var bills = [124, 48, 268];
 
-    if (bills <= 50) {
-        console.log (bills * 1.2);
-    } else if (bills > 50 && bills <= 200) {
-        console.log (bills * 1.15);
-    } else if (bills > 200) {
-        console.log (bills * 1.1);
-    }
+if (bills <= 50) {
+    console.log(bills * 1.2);
+} else if (bills > 50 && bills <= 200) {
+    console.log(bills * 1.15);
+} else if (bills > 200) {
+    console.log(bills * 1.1);
+}
 
 function tipcalculator(bill) {
     var percentage;
@@ -55,24 +55,26 @@ function tipcalculator(bill) {
 
 var bills = [124, 48, 268];
 var tips = [tipcalculator(bills[0]),
-            tipcalculator(bills[1]),
-            tipcalculator(bills[2])];
+    tipcalculator(bills[1]),
+    tipcalculator(bills[2])
+];
 
 var finalValues = [bills[0] + tips[0],
-                    bills[1] + tips[1],
-                    bills[2] + tips[2]];
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+];
 
 
 console.log(tips);
 
 
-function tipCaliculator(bill){
+function tipCaliculator(bill) {
     var tip;
-    if (bill < 50 ) {
+    if (bill < 50) {
         tip = 0.2;
     } else if (bill > 50 && bill < 200) {
         tip = 0.15;
-    }else if (bills > 200) {
+    } else if (bills > 200) {
         tip = 0.12;
     }
 
@@ -82,12 +84,14 @@ function tipCaliculator(bill){
 
 var bills = [124, 48, 268];
 var allTips = [tipCaliculator(bills[0]),
-                tipCaliculator(bills[1]),
-                tipCaliculator(bills[2])];
+    tipCaliculator(bills[1]),
+    tipCaliculator(bills[2])
+];
 
 const totalValue = [bills[0] + allTips[0],
-                    bills[1] + allTips[1],
-                    bills[2] + allTips[2]];
+    bills[1] + allTips[1],
+    bills[2] + allTips[2]
+];
 
 console.log(allTips);
 
@@ -95,12 +99,12 @@ console.log(allTips);
 //objects and properties 
 
 var john = {
-    firstName: 'John', 
+    firstName: 'John',
     lastName: 'Smith',
     birthYear: '1990',
-    family:['Jane', 'Mark', 'bob', 'emily'],
-    job:'teacher',
-    isMarried:'false'
+    family: ['Jane', 'Mark', 'bob', 'emily'],
+    job: 'teacher',
+    isMarried: 'false'
 };
 
 console.log(john);
@@ -118,12 +122,12 @@ jane['lastname'] = 'Smith';
 console.log(jane);
 
 var john = {
-    firstName: 'John', 
+    firstName: 'John',
     lastName: 'Smith',
     birthYear: '1990',
-    family:['Jane', 'Mark', 'bob', 'emily'],
-    job:'teacher',
-    isMarried:'false',
+    family: ['Jane', 'Mark', 'bob', 'emily'],
+    job: 'teacher',
+    isMarried: 'false',
     calcAge: function() {
         this.age = 2018 - this.birthYear;
     }
@@ -136,25 +140,69 @@ console.log(john);
 var john = {
     fullName: "John Smith",
     mass: 92,
-    height: 1.95, 
-    calcBMI: function(){
-        this.bmi = this.mass / (this.mass * this.height); 
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
     }
 };
 
 var Mark = {
     fullName: "Mark Stephan",
     mass: 78,
-    height: 1.69, 
-    BMI: function(){
-        this.mass / this.height^2; 
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
     }
 };
 
-john.calcBMI();
-console.log(john);
+console.log(john.calcBMI(), Mark.calcBMI());
 
+if (john.calcBMI() > Mark.calcBMI()) {
+    console.log(john.fullName, john.calcBMI())
+} else {
+    console.log(Mark.fullName, Mark.calcBMI());
+};
 
+// loops and iteration 
 
+for (var i = 0; i < 1000; i += 13) {
+    console.log(i);
+}
 
+var john = ['John', 'Smith', 1990, 'designer', 'false'];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+};
 
+var i = 0;
+while (i < john.length) {
+    console.log(john[1]);
+    i++;
+}
+
+//continue and break statesments
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+};
+
+for (var i = 0; i < 1000; i += 13) {
+    if (typeof i === 11 * i) continue;
+    console.log(i);
+}
+
+var fibonacci_series = function(n) {
+    if (n === 1) {
+        return [0, 1];
+    } else {
+        var s = fibonacci_series(n - 1);
+        s.push(s[s.length - 1] + s[s.length - 2]);
+        return s;
+    }
+};
+
+console.log(fibonacci_series(10));
