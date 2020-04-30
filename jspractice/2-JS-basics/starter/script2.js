a4aefe4b0bf93aebaa1915ffbaf4d0b031858bc7
Ryosuke Oshida <ryosuke-oshida@cybozu.co.jp>
2020-04-30 22:43:49 +0900

    sup

--- jspractice/2-JS-basics/starter/script2.js
+++ jspractice/2-JS-basics/starter/script2.js
@@ -228,3 +228,48 @@ function tipCalculator2() {
 };
 
 console.log(tipCalculator2());
+
+function toCelsius(f){
+    return 3 * (f-30);
+}
+
+console.log(toCelsius(60));
+
+var john = {
+    fullName : 'John Smith',
+    bills : [124, 48, 268, 180, 42], 
+    calTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++)
+        {
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 50) {
+                percentage = .2;
+            }else if (bill >= 50 && bill < 200) {
+                percentage = .15;
+            }else {
+                percentage = -1;
+            }
+
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+john.calTips();
+console.log(john);
+console.log(john.tips[]);
+
+function sumTips() {
+    for (var i = 0; i < john.tips[i]; i++)
+    {
+        john.tips
+    }
+}
+
+console.log(sumTips());
